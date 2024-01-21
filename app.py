8f32d1ec00d67b8a171f9abfec1265c1c56c62ac
Biraj Karki <birajkarki9849@gmail.com>
2024-01-21 13:02:55 +0545

    basic routes in flask

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, url_for
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
